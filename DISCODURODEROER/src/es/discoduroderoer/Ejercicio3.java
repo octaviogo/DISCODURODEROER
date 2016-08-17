c8e58b7b978030fab85478207c3cc819dbf5a269
gunsofledz <gunso@test.mx>
2016-08-16 22:41:46 -0500

    Teset de commit

---
+++ DISCODURODEROER/src/es/discoduroderoer/Ejercicio3.java
@@ -0,0 +1,73 @@
+package es.discoduroderoer;
+
+public class Ejercicio3 {
+
+	private int arr0[];
+	private int media = 0;
+	
+	
+	public static void main(String[] args) {
+		Ejercicio3 ej = new Ejercicio3();
+		ej.cargaArreglo();
+		ej.calculaMedia();
+		ej.calculaMediana();
+	}
+	
+	
+	private void cargaArreglo(){
+		arr0 = new int[100];
+		
+		for (int i = 0; i < arr0.length; i++) {
+			
+			arr0[i]= ((int) Math.floor(Math.random()*(0-100)*-1));
+		}
+		
+		for (int j = 0; j < arr0.length; j++) {
+			
+		System.out.println( j + "["+ arr0[j] +"]");
+		}
+		
+		
+	}
+	
+	private void calculaMedia(){
+		
+	
+		for (int i = 0; i < arr0.length; i++) {
+			media = media + arr0[i];
+		}
+		
+		
+		System.out.println("La media del vector es: "+(media/arr0.length));
+	}
+	
+	private void calculaMediana(){
+		
+		System.out.println("El valor de la mediana del vector es: " + arr0[arr0.length/2]);
+		
+	}
+	
+	private void calculaModa(){
+		
+		System.out.println("::Calcular Moda::");
+		System.out.println("¿Cuantos numeros desea buscar? ");
+		
+		
+		int arr01[] = {8,9,10};
+		
+		
+		for (int i = 0; i < arr0.length; i++) {
+			for (int j = 0; j < arr0.length; j++) {
+				if(arr0[i] == arr0[j]){
+					
+					
+				} 
+			}
+		}
+		
+		
+		System.out.println("La moda del vector es: ");
+		
+		
+	}
+}
